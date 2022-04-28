340ef99c0e0ebcdfa592820ed02ac208480c4d14
papitz <paul@familie-hoeppner.de>
2022-04-28 21:35:56 +0200

    can now read files from commandline

--- genderbot.py
+++ genderbot.py
@@ -1,5 +1,7 @@
 import csv
 import string
+import sys
+from termcolor import colored
 
 dictonary = {}
 with open("words.csv") as wordlist:
@@ -11,19 +13,21 @@ with open("words.csv") as wordlist:
         else:
             dictonary.update({row[0].casefold(): row[1].split(";")})
 
-print(dictonary)
+input_files = sys.argv[1:]
 
-with open("grundlagen.tex") as input:
-    lines = input.readlines()
-    line_number = 0
-    for line in lines:
-        line_number += 1
-        line_without_punctuation = line.translate(
-            str.maketrans("", "", string.punctuation)
-        )
-        words = line_without_punctuation.split()
-        for word in words:
-            if word.casefold() in dictonary:
-                print(
-                    f"{word} in Zeile {line_number} ist nicht gegendert. Alternativen: {dictonary[word.casefold()]}"
-                )
+for file in input_files:
+    print(colored(f"\nReading from file {file}\n", "green"))
+    with open(file) as input:
+        lines = input.readlines()
+        line_number = 0
+        for line in lines:
+            line_number += 1
+            line_without_punctuation = line.translate(
+                str.maketrans("", "", string.punctuation)
+            )
+            words = line_without_punctuation.split()
+            for word in words:
+                if word.casefold() in dictonary:
+                    print(
+                        f"{word} in Zeile {line_number} ist nicht gegendert. Alternativen: {dictonary[word.casefold()]}"
+                    )
